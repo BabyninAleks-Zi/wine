fd7335156535a3073792293e81845efe908c06d4
Aleksandr Babynin <babynin.home@gmail.com>
2026-01-28 17:06:34 +1100

    Обновил скрипт в соответствии с рекомендациями
    1) Вынес рендер страницы в отдельную функцию
    2) Вывел основную логику в ifmain
    3) Добавил возможность ввести путь к файлу данных, либо оставить в корне по дефолту.

--- main.py
+++ main.py
@@ -3,18 +3,26 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from collections import defaultdict
 from datetime import date
 import pandas
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
-company_age = date.today().year-1920
+import argparse
 
 
-def years_word(company_age):
+def build_page(wine_database):
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+
+    company_age = date.today().year - 1920
+
+    return template.render(
+        company_age=company_age,
+        years_word=get_years_word(company_age),
+        wines=get_wines(wine_database)
+    )
+
+
+def get_years_word(company_age):
     if 11 <= company_age % 100 <= 14:
         return 'лет'
     if company_age % 10 == 1:
@@ -24,8 +32,8 @@ def years_word(company_age):
     return 'лет'
 
 
-def get_wines():
-    wines_data = pandas.read_excel('wine3.xlsx')
+def get_wines(wine_database):
+    wines_data = pandas.read_excel(wine_database)
     wines_data = wines_data.fillna('')
     grouped_wines = defaultdict(list)
     for _, row in wines_data.iterrows():
@@ -39,14 +47,23 @@ def get_wines():
     return dict(grouped_wines)
 
 
-rendered_page = template.render(
-    company_age=company_age,
-    years_word=years_word(company_age),
-    wines=get_wines()
-)
+def main():
+    parser = argparse.ArgumentParser(
+        description='Сайт винодельни, введите путь к файлу с данными о винах'
+    )
+    parser.add_argument(
+        '--wine_database',
+        help='Путь к файлу с данными о винах',
+        default='wine.xlsx'
+    )
+    args = parser.parse_args()
+    wine_database = args.wine_database
+    rendered_page = build_page(wine_database)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ == '__main__':
+    main()
